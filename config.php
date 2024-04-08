b8f66c1771023820c933a349b27be12e7cad4667
Theophilus Lincoln <lincolntheop@gmail.com>
2024-04-08 11:14:37 +0300

    Removed database credentials

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Database credentials
-$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = 'southACT41#8$';
-$dbName = '2fa_db';
+$dbHost = '';
+$dbUsername = '';
+$dbPassword = '';
+$dbName = '';
 
 ?>
